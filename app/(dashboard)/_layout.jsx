f82bb70b5878cb5348e35fbff7495a2c6f43ae00
Aditya Alshi <alshiaditya55@gmail.com>
2026-05-11 14:36:30 +0530

    refactor
    Improved tabs names and appearence
    
    Applied themed colors to  the tabs. Added Tab staks and set options on individual tabs Using Tab.Screen options settings

--- app/(dashboard)/_layout.jsx
+++ app/(dashboard)/_layout.jsx
@@ -1,10 +1,40 @@
-import { View, Text } from 'react-native'
+import { View, Text, useColorScheme } from 'react-native'
 import React from 'react'
 import { Tabs } from 'expo-router'
 
+import { Colors } from "../../constants/Colors"
+
 const DahsboardLayout = () => {
+  const colorScheme = useColorScheme();
+  const theme = Colors[colorScheme] ?? Colors.light;
+
+
   return (
-    <Tabs></Tabs>
+    <Tabs 
+      screenOptions={{
+        headerShown: false,
+        tabBarStyle: {
+          backgroundColor: theme.navBackground,
+          paddingTop: 10,
+          height: 90
+        },
+        tabBarActiveTintColor: theme.iconColorFocused,
+        tabBarInactiveTintColor: theme.iconColor
+      }}
+    >
+      <Tabs.Screen 
+        name="profile"
+        options={{title: "Profile"}}
+      />
+      <Tabs.Screen 
+        name="books"
+        options={{title: "Books"}}
+      />
+      <Tabs.Screen 
+        name="create"
+        options={{title: "Create"}}
+      />
+    </Tabs>
   )
 }
 
